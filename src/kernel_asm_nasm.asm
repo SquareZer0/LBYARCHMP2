368d7bc8ea0770d98ecd76389bb87eb71cc6916e
SquareZer0 <miguel_angelo_ignacio@dlsu.edu.ph>
2026-08-03 22:20:00 +0800

    feat: implement vector distance calculation in x86-64 NASM assembly

--- src/kernel_asm_nasm.asm
+++ src/kernel_asm_nasm.asm
@@ -1,46 +1,60 @@
-; kernel_asm_nasm.asm
-; void distance_asm(int n, double* X1, double* X2, double* Y1, double* Y2, double* Z)
-; Computes Z[i] = sqrt((X2[i]-X1[i])^2 + (Y2[i]-Y1[i])^2) using scalar SIMD instructions
+;S25I - Miguel Ignacio
 
 section .text
 global distance_asm
 
 distance_asm:
-    ; Arguments (Windows x64 ABI):
-    ; ecx = n
-    ; rdx = X1
-    ; r8  = X2
-    ; r9  = Y1
-    ; [rsp+40] = Y2
-    ; [rsp+48] = Z
+        ; ---- grab stack-passed args FIRST, before any push shifts rsp ----
+        mov     r10, [rsp+40]       ; r10 = Y2
+        mov     r11, [rsp+48]       ; r11 = Z
 
-    mov     r10, [rsp+40]               ; Y2
-    mov     r11, [rsp+48]               ; Z
+        ; ---- save non-volatile registers we're about to use ----
+        push    rbx
+        push    rsi
+        push    rdi
+        push    r12
+        push    r13
+        push    r14
 
-    xor     rax, rax                    ; i = 0
+        ; ---- move parameters into stable, non-volatile registers ----
+        mov     ebx, ecx            ; ebx = n
+        mov     rsi, rdx            ; rsi = X1
+        mov     rdi, r8             ; rdi = X2
+        mov     r12, r9             ; r12 = Y1
+        mov     r13, r10            ; r13 = Y2
+        mov     r14, r11            ; r14 = Z
+
+        xor     eax, eax            ; eax = i = 0
+        test    ebx, ebx
+        jle     .done               ; if n <= 0, nothing to do
 
 .loop_start:
-    cmp     eax, ecx
-    jge     .done
+        cmp     eax, ebx
+        jge     .done
 
-    ; dx = X2[i] - X1[i]
-    movsd   xmm0, [r8 + rax*8]
-    subsd   xmm0, [rdx + rax*8]
-    mulsd   xmm0, xmm0                  ; dx^2
+        ; ---- dx = X2[i] - X1[i]; dx2 = dx*dx ----
+        movsd   xmm0, [rdi+rax*8]   ; xmm0 = X2[i]
+        subsd   xmm0, [rsi+rax*8]   ; xmm0 = X2[i] - X1[i]
+        mulsd   xmm0, xmm0          ; xmm0 = dx*dx
 
-    ; dy = Y2[i] - Y1[i]
-    movsd   xmm1, [r10 + rax*8]
-    subsd   xmm1, [r9 + rax*8]
-    mulsd   xmm1, xmm1                  ; dy^2
+        ; ---- dy = Y2[i] - Y1[i]; dy2 = dy*dy ----
+        movsd   xmm1, [r13+rax*8]   ; xmm1 = Y2[i]
+        subsd   xmm1, [r12+rax*8]   ; xmm1 = Y2[i] - Y1[i]
+        mulsd   xmm1, xmm1          ; xmm1 = dy*dy
 
-    ; Z[i] = sqrt(dx^2 + dy^2)
-    addsd   xmm0, xmm1
-    sqrtsd  xmm0, xmm0
-    movsd   [r11 + rax*8], xmm0
+        ; ---- Z[i] = sqrt(dx2 + dy2) ----
+        addsd   xmm0, xmm1          ; xmm0 = dx*dx + dy*dy
+        sqrtsd  xmm0, xmm0          ; xmm0 = sqrt(...)
+        movsd   [r14+rax*8], xmm0   ; Z[i] = xmm0
 
-    inc     eax
-    jmp     .loop_start
+        inc     eax
+        jmp     .loop_start
 
 .done:
-    ret
-
+        pop     r14
+        pop     r13
+        pop     r12
+        pop     rdi
+        pop     rsi
+        pop     rbx
+        ret
